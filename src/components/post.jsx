c7a7ceb4152f6b4bcb049b4c24f36416a6b71db8
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2023-07-01 21:32:47 +0800

    Added the comment and like count

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -27,17 +27,34 @@ export default function Post({ postInfo }) {
   }
 
   return (
-    <div className='w-full p-4 border-[#464b5f] border-b-[1px] '>
-        <div>
-            <div>{postInfo.author.username}</div>
+    <div className='w-full p-4 border-[#464b5f] border-b-[1px] text-white flex flex-col'>
+        <div className='w-full flex justify-between items-center'>
+          <div>
+            {/* <img src={postInfo.author.profilePicture} alt="Profile Picture" className='w-10 h-10 rounded-full'/> */}
+            <div className='font-bold'>{postInfo.author.username}</div>
+          </div>
             <div>{timeStamp(postInfo.createdAt)}</div>
         </div>
-        <div>{postInfo.content}</div>
+        <div className='text-sm'>{postInfo.content}</div>
         <div>Images</div>
-        <div>
-            <button>Like</button>
-            <button>Comment</button>
-            <button>Share</button>
+        <div className='w-1/3 flex justify-between place-self-end'>
+            <button className='hover:bg-[#99acc633] p-1 rounded-md flex' >
+                <span className='pr-2'>{postInfo.likes.length}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+  <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" />
+                </svg>
+            </button>
+            <button className='hover:bg-[#99acc633] p-1 rounded-md flex'>
+              <span className='pr-2'>{postInfo.comments.length}</span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+  <path fillRule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z" clipRule="evenodd" />
+              </svg>
+            </button>
+            <button className='hover:bg-[#99acc633] p-1 rounded-md'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+  <path fillRule="evenodd" d="M15.75 4.5a3 3 0 11.825 2.066l-8.421 4.679a3.002 3.002 0 010 1.51l8.421 4.679a3 3 0 11-.729 1.31l-8.421-4.678a3 3 0 110-4.132l8.421-4.679a3 3 0 01-.096-.755z" clipRule="evenodd" />
+              </svg>
+            </button>
         </div>
     </div>
   )
