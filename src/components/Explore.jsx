f50b138cb52962e9a2eefac9d85be48793b6b5d5
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2023-07-19 23:46:06 +0800

    Started improving the explore

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,35 +1,71 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import useAuth from '../authentication/useAuth';
+import { useState, useEffect, useRef } from 'react';
+import Spinner from './Spinner';
 import * as postAPI from '../api/postAPI';
-
 import Post from './Post';
-import PostForm from './PostForm';
 import PostSingle from './PostSingle';
 
 const Explore = () => {
-    const { user, isLoading, logout, error} = useAuth();
-
     const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
     const [posts, setPosts] = useState([]);
-
     const [ showPost, setShowPost ] = useState(null);
-
     const [errors,setErrors] = useState(null);
+    const [stopFetching, setStopFetching] = useState(false); 
+    const containerRef = useRef(null);
 
     useEffect(()=>{
         fetchPosts();
     }, []);
 
-    const fetchPosts = async(pageOpt=page) => {
-        const res = await postAPI.getPosts(pageOpt, true);
+    useEffect(() => {
+    
+        const container = containerRef.current;
+    
+        const options = {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0.1,
+        };
+    
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            console.log(entry.isIntersecting);
+            if (entry.isIntersecting) {
+              setIsLoading(true);
+              fetchPosts()
+                .finally(() => setIsLoading(false));
+            }
+          },
+          options
+        );
+    
+        if (container) {
+          observer.observe(container);
+        }
+    
+        return () => {
+          observer.disconnect();
+        };
+      }, [posts]);
+
+      const fetchPosts = async() => {
+        
+        if (stopFetching) {
+            return;
+        }
+
+        const res = await postAPI.getPosts(page, true);
         if (res.status === 'success') {
+            if (res.data.length === 0) {
+                setStopFetching(true);
+                return;
+            }
+
             const postEle = res.data.map((post) => {
                 return (<Post key={post._id} postInfo={post} setShowPost={setShowPost} />);
             });
-            setPosts(postEle);
-            setPage(pageOpt+1);
+            setPosts([...posts, ...postEle]);
+            setPage(page+1);
             return;
         }
         setErrors("Could not fetch posts");
@@ -50,7 +86,18 @@ const Explore = () => {
                     <span className='text-xl text-white'>Explore</span>
                 </h3>
                 <div className=' flex flex-col w-full flex-grow overflow-y-scroll scrollbar:bg-blue-500 rounded-xl scrollbar scrollbar-thumb-blue-500 scrollbar-track-gray-200'>
-                    {posts ? posts : <div>No posts</div>}
+                    {errors && <div className='text-red-500 text-center'>{errors}</div>}
+                    {posts && posts.length === 0 && <div className='text-center text-white'>No posts</div>}
+                    {posts.length > 0 && 
+                        <>
+                            {posts}
+                            {isLoading ? 
+                              <Spinner size={12} /> 
+                            :
+                              <div ref={containerRef} className='border-[1px] border-transparent'></div>
+                            }
+                        </>
+                    }
                 </div>
             </>
             }
